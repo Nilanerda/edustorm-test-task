a06f25dbd6b4e3e8aa69cb74a761f3c377321721
Yana <zzizzu@ukr.net>
2020-02-06 15:55:35 +0200

    configured environment, created basic markup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/javascript/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+                        {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        })
+
+    ],
+    mode: 'development'
+};
